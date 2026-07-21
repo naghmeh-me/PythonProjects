51847686bd50e0e1b17147ecf5c74b32e01527c1
naghmeh-me <naghmehmehrnoosh@gmail.com>
2026-07-22 01:50:15 +0330

    Update nemone 6.py

--- All Codes/53/nemone 6.py
+++ All Codes/53/nemone 6.py
@@ -1,20 +1,6 @@
+#"Write a program that takes x and y as input, and uses an anonymous (lambda) function to compute and display x raised to the power of y plus y raised to the power of x."
+
 #1
-from math import pow
-
-def_=lambda x,y:pow(x,y)+pow(y,x)
-
-while True:
-    try:
-        x=int(input("enter a number:"))
-        y=int(input("enter a number:"))
-    except:
-        print("erroe! enter valid information.")
-        continue
-    else:
-        print(def_(x,y))
-        break
-
-#2
 z=lambda x,y:x**y+y**x
 
 x=int(input("enter a number:"))
@@ -22,4 +8,24 @@ y=int(input("enter a number:"))
 
 print(z(x,y))
 
+#2
+from math import pow
 
+print("For exit from the program enter 0 for value of x.")
+
+while True:
+    try:
+        x=int(input("Enter value of x:"))
+        
+        if x==0:
+            break
+        
+        y=int(input("Enter value of y:"))
+
+    except ValueError:
+        print("Error! Please enter CORRECT NUMBERS.")
+        continue
+
+    else:
+        function_=lambda x,y:pow(x,y)+pow(y,x)
+        print(f"Answer: {function_(x,y)}")
